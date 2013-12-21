8c86ef212b069e2e8e83f991411e5c14f35d7b1e
Anthony Cannistra <tony.cannistra@gmail.com>
2013-12-21 01:24:45 -0500

    documenting

--- server/doorman/models.py
+++ server/doorman/models.py
@@ -4,7 +4,10 @@ from django.contrib.auth.models import User, UserManager
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-
+'''
+	RFIDTag is a representation of an RFID device. Each RFID Tag is
+	associated with a single user.
+'''
 class RFIDTag(models.Model):
 	tag     = models.CharField(max_length=200)
 	user    = models.ForeignKey(User)
@@ -12,8 +15,12 @@ class RFIDTag(models.Model):
 	def __unicode__(self):
 		return (self.user.username+"_"+self.tag).strip().replace(" ", "")
 
+'''
+	A CheckIn represents a single actuation of the RFID reader. A timestamp
+	(currently server-recorded, 12/21/13) and a user is associated. 
 
-
+	TODO: Do we want to keep all checkins? 
+'''
 class CheckIn(models.Model):
 	time     = models.DateTimeField(auto_now=False, auto_now_add=True)
 	user     = models.ForeignKey(User)
@@ -21,7 +28,17 @@ class CheckIn(models.Model):
 	def __unicode__(self):
 		return self.user.username+'@'+self.time.strftime('%x_%X')
 
+'''
+	** Note. This is *NOT* how user data should be handled in Django. 
+	If I wasn't pressed for time this would certainly be encompassed in a 
+	subclass of django.contrib.auth.models.User.
 
+	UserProfile adds additional data to each user. It holds onto the most recentCheckin, 
+	whether or not the user is home, and their RFIDTags
+
+	TODO: Figure out if that RFID tag parameter is necessary even a little.
+
+'''
 class UserProfile(models.Model):
 	user = models.ForeignKey(User, unique=True)
 	recentCheckin = models.ForeignKey(CheckIn, null=True)
@@ -34,16 +51,14 @@ class UserProfile(models.Model):
 	def togglePresent(self):
 		self.isPresent = not self.isPresent
 
+# Here are Django Signal handlers, actuated when instances of the above models
+# are created. 
 
-
-# post_save handler for users
-# @receiver(post_save, sender=User)
-# def makeIsPresent(sender, created, instance, **kwargs):
-# 	if(created):
-# 		ip = isPresent(isPresent=True, user=instance)
-# 		ip.save()
-# 		print('created new boolean for user ' + instance.username)
-
+'''
+	Actuated on creation of a *new* checkin. 
+	* Toggles user's "is present" parameter in User Profile
+	* Adds itself to most recent checkin parameter in corresponding user profile
+'''
 @receiver(post_save, sender=CheckIn)
 def toggle(sender, created, instance, **kwargs):
 	if created:
@@ -53,7 +68,10 @@ def toggle(sender, created, instance, **kwargs):
 		profile.save()
 
 		
-
+'''
+	Creates a new user profile to correspond with the creation of 
+	each new user by Django's auth modules. 
+'''
 @receiver(post_save, sender=User)
 def makeProfile(sender, created, instance, **kwargs):
 	if created:
